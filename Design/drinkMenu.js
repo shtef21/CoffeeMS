0e2869acf49f8f13632ef3fb8f2326c89a0dedca
shtef21 <stjepan.salopek@hotmail.com>
2023-10-16 16:48:57 +0200

    CSS & JS refactor
    Move CSS to SASS
    Create JSON (TODO - use it to generate tables)

---
+++ Design/drinkMenu.js
@@ -0,0 +1,146 @@
+var drinkMenu = [
+    {
+        "category": "Coffee",
+        "items": [
+            {
+                "name": "Espresso",
+                "price": 1.4
+            },
+            {
+                "name": "Cappuccino",
+                "price": 1.97
+            },
+            {
+                "name": "Latte",
+                "price": 1.77
+            },
+            {
+                "name": "Americano",
+                "price": 1.59
+            },
+            {
+                "name": "Mocha",
+                "price": 2.07
+            },
+            {
+                "name": "Macchiato",
+                "price": 1.7
+            }
+        ]
+    },
+    {
+        "category": "Tea",
+        "items": [
+            {
+                "name": "Green Tea",
+                "price": 1.18
+            },
+            {
+                "name": "Earl Grey",
+                "price": 1.33
+            },
+            {
+                "name": "Chai Tea",
+                "price": 1.48
+            },
+            {
+                "name": "Herbal Tea",
+                "price": 1.12
+            }
+        ]
+    },
+    {
+        "category": "Espresso",
+        "items": [
+            {
+                "name": "Single Espresso",
+                "price": 1.13
+            },
+            {
+                "name": "Double Espresso",
+                "price": 1.43
+            }
+        ]
+    },
+    {
+        "category": "Smoothies",
+        "items": [
+            {
+                "name": "Strawberry Banana Smoothie",
+                "price": 1.68
+            },
+            {
+                "name": "Mango Tango Smoothie",
+                "price": 1.59
+            }
+        ]
+    },
+    {
+        "category": "Pastries",
+        "items": [
+            {
+                "name": "Croissant",
+                "price": 0.84
+            },
+            {
+                "name": "Muffin",
+                "price": 1.03
+            },
+            {
+                "name": "Danish Pastry",
+                "price": 1.2
+            },
+            {
+                "name": "Scone",
+                "price": 0.99
+            }
+        ]
+    },
+    {
+        "category": "Milk Options",
+        "items": [
+            {
+                "name": "Almond Milk",
+                "price": 0.35
+            },
+            {
+                "name": "Soy Milk",
+                "price": 0.42
+            },
+            {
+                "name": "Oat Milk",
+                "price": 0.39
+            }
+        ]
+    },
+    {
+        "category": "Flavorings",
+        "items": [
+            {
+                "name": "Vanilla Syrup",
+                "price": 0.25
+            },
+            {
+                "name": "Caramel Syrup",
+                "price": 0.3
+            },
+            {
+                "name": "Hazelnut Syrup",
+                "price": 0.32
+            }
+        ]
+    },
+    {
+        "category": "Iced Drinks",
+        "items": [
+            {
+                "name": "Iced Coffee",
+                "price": 1.63
+            },
+            {
+                "name": "Iced Tea",
+                "price": 1.39
+            }
+        ]
+    }
+]
