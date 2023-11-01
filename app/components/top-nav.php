01a6b08ae3cf0d2240f0bdfb9d24720f01ba0089
shtef21 <stjepan.salopek@hotmail.com>
2023-11-01 19:56:45 +0100

    Topbar tweak

--- app/components/top-nav.php
+++ app/components/top-nav.php
@@ -1,20 +1,18 @@
 <div class="site-nav">
     <a href="?p=home">Home</a>
-    <a href="?p=login">Login</a>
-    <a href="?p=register">Register</a>
     <a href="?p=about_us">About us</a>
 
     <?php
     if($_SESSION["role"] >= 2) {
         echo '<a href="?p=admin">Admin</a>';
     }
-    if($_SESSION["role"] >= 1
-        || true  // Remove after debugging done
-    ) {
+    if($_SESSION["role"] >= 1) {
         echo '<a href="?p=search_drink">Find a cocktail</a>';
     }
     if($_SESSION["logged_in"]) {
-        echo '<a href="?p=logout" class="disabled">Log out</a>';
+        echo '<a href="?p=logout" class="disabled">Log out (' . $_SESSION["username"] . ')</a>';
+    } else {
+        echo '<a href="?p=login">Login</a>' . '<a href="?p=register">Register</a>';
     }
     ?>
     
